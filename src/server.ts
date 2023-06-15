9093cbe93d484cf2537283b88362de5105dafc25
Randi Guarcas <rguarcas18@gmail.com>
2023-06-15 13:15:27 -0600

    Added cors support

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,13 @@
-import express, { Express, Request, Response } from "express";
+import express, { Express } from "express";
 import { taskRoute, welcomeRoute } from "./routes";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 const app: Express = express();
 const port = process.env.PORT || 3000;
 
+app.use(cors());
 app.use(express.json());
 
 app.use("/", welcomeRoute);
