472f0fc859359d110b0d3bab725db07795aa89e2
Randi Guarcas <rguarcas18@gmail.com>
2023-06-15 21:06:26 -0600

    remove braek dependencies

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,6 @@ import express, { Express } from "express";
 import { taskRoute, welcomeRoute } from "./routes";
 import dotenv from "dotenv";
 import cors from "cors";
-import swaggerUi from "swagger-ui-express";
-
-const swaggerDocument = require("./swagger.json");
 
 dotenv.config();
 const app: Express = express();
@@ -15,7 +12,6 @@ app.use(express.json());
 
 app.use("/", welcomeRoute);
 app.use("/tasks", taskRoute);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`Server running at https://localhost:${port} 🚀`);
