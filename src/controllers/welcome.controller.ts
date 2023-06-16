d50d99b81ada0728144584a83aed5bb4ef2e8cab
Randi Guarcas <rguarcas18@gmail.com>
2023-06-15 21:31:10 -0600

    Added fix on redirect

--- src/controllers/welcome.controller.ts
+++ src/controllers/welcome.controller.ts
@@ -3,8 +3,8 @@ const welcomeAction = (): string => {
     <h2>Welcome to my Atom challenge</h2>
     <p>
       <code>Hello, my name is Randi Guarcas</code><br/><br/>
-      <code>Please visit project repository <a href="https://github.com/randiguarcas/atom-challenge">here</a></code><br/><br/>
-      <code>Please visit postman collection <a href="https://xrdg-public.postman.co/">here</a></code>
+      <code>Please visit project repository <a target="__blank" href="https://github.com/randiguarcas/atom-challenge">here</a></code><br/><br/>
+      <code>Please visit postman collection <a target="__blank" href="https://documenter.getpostman.com/view/252408/2s93sgYBHg">here</a></code>
     </p>
   `;
 };
