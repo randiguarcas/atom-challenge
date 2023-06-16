47ac3bda26a33eb7ed6f8d675b96df27a22de403
Randi Guarcas <rguarcas18@gmail.com>
2023-06-15 20:54:04 -0600

    Added swagger

--- src/controllers/welcome.controller.ts
+++ src/controllers/welcome.controller.ts
@@ -1,5 +1,12 @@
 const welcomeAction = (): string => {
-  return "Welcome to my Atom Challenge";
+  return `
+    <h2>Welcome to my Atom challenge</h2>
+    <p>
+      <code>Hello, my name is Randi Guarcas</code><br/><br/>
+      <code>Please visit project repository <a href="https://github.com/randiguarcas/atom-challenge">here</a></code><br/><br/>
+      <code>Please visit postman collection <a href="https://xrdg-public.postman.co/">here</a></code>
+    </p>
+  `;
 };
 
 export default {
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import express, { Express } from "express";
 import { taskRoute, welcomeRoute } from "./routes";
 import dotenv from "dotenv";
 import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+
+const swaggerDocument = require("./swagger.json");
 
 dotenv.config();
 const app: Express = express();
@@ -12,6 +15,7 @@ app.use(express.json());
 
 app.use("/", welcomeRoute);
 app.use("/tasks", taskRoute);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(port, () => {
   console.log(`Server running at https://localhost:${port} 🚀`);
